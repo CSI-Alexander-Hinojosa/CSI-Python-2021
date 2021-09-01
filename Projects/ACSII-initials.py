d4ae2b274d1de9d5b156aea88b994092ea1462d5
CSI-Alexander-Hinojosa <25-022@sanignacio.pr>
2021-09-01 14:47:30 -0400

    Entrega de proyecto de iniciales.

---
+++ Projects/ACSII-initials.py
@@ -0,0 +1,9 @@
+print("""
+   A      L      EEEEE  X     X
+ A   A    L      E      X     X
+A     A   L      E       X   X
+A AAA A   L      EEE       X
+A     A   L      E       X   X
+A     A   L      E      X     X
+A     A   LLLLL  EEEEE  X     X
+""")
