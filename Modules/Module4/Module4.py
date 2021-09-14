742bb96d083ab26023d7f43e81a8cd55ebd38820
CSI-Alexander-Hinojosa <25-022@sanignacio.pr>
2021-09-14 15:10:28 -0400

    Module 4 answers

--- Modules/Module4/Module4.py
+++ Modules/Module4/Module4.py
@@ -1,9 +1,10 @@
-print("What did you have for Breakfast?")
-meal= "Cereal"
-print(meal)
-print("What was your lunch?")
-meal= "Nachos with cheese"
-print(meal)
-print("Have you had dinner?")
-meal= "Not yet"
-print(meal)
+# print("What did you have for Breakfast?")
+# meal= "Cereal"
+# print(meal)
+# print("What was your lunch?")
+# meal= "Nachos with cheese"
+# print(meal)
+# print("Have you had dinner?")
+# meal= "Not yet"
+# print(meal)
+
