fb7110b0d1dcc0dfb93867b0a0a44ef072e6aaa1
CSI-Alexander-Hinojosa <25-022@sanignacio.pr>
2021-10-30 21:59:49 -0400

    Update from the ProyectileMotion file.

--- Projects/Projectile-Motion/Alexander-Hinojosa-ProyectileMotion.py
+++ Projects/Projectile-Motion/Alexander-Hinojosa-ProyectileMotion.py
@@ -9,6 +9,8 @@ import math
 
 # gravity_Ms = 9.81
 
+print("Welcome to the proyectile motion analisis. On this file, it would calculate the motion of the proyectile from a weapon. The weapom selected is the AS VAL, with an ammunition of 9x39mm SSP gs with a velocity of 310. The proyectile is going to be expelled from the Ocean Tower from a height of 243ft.")
+
 def ProyectileFunction(gun:str, caliber:str, ammunition:str, velocity_ms:int, Building:str, BuildingHeight:str, gravity_Ms:int):
     time_s= math.sqrt(2*BuildingHeight/gravity_Ms)
     distance= (velocity_ms*gravity_Ms)
