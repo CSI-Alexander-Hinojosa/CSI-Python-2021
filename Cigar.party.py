59edfe208e801386fc540dbe8b22d8ea73d6e49f
CSI-Alexander-Hinojosa <25-022@sanignacio.pr>
2021-12-01 10:38:44 -0400

    Update of project of cigar party

--- Cigar.party.py
+++ Cigar.party.py
@@ -1,19 +1,36 @@
 def cigar_party(cigars, is_weekend):
+  if is_weekend:
+    if cigars >= 40:
+      return True
+  else:
+    if cigars >= 40 and cigars <= 60:
+      return True
+  return False
 
-    if is_weekend:
-        print(40-60, True)
-    else:
-        print(cigar>60,False)
 
-cigar:float = float(input("What is the amount of cigars:"))
+# def cigar_party(cigars, is_weekend):
+#   if is_weekend:
+#     if cigars >=40:
+#         return True 
+#   elif cigars <=60:
+#       return True
+#   else:
+#         return False
 
-def cigar_party(cigar, is_week):
-    if (cigar<40):
-        print(False)
-    elif (cigar>=40):
-        print(True)
-    elif (cigar>60):
-        print(False)
+#     if is_weekend:
+#         print(40-60, True)
+#     else:
+#         print(cigar>60,False)
+
+# cigar:float = float(input("What is the amount of cigars:"))
+
+# def cigar_party(cigar, is_week):
+#     if (cigar<40):
+#         print(False)
+#     elif (cigar>=40):
+#         print(True)
+#     elif (cigar>60):
+#         print(False)
 
 # # ground shipping
 # if (weight == 2 or weight < 2 ):
@@ -28,3 +45,4 @@ def cigar_party(cigar, is_week):
 # else: 
 #     cost_ground=weight*5.25+20
 #     print("Ground shipping: $", float(cost_ground))
+
