2311af9b61cb3016e2b2efd9c8e45850a1ba4f8e
CSI-Alexander-Hinojosa <25-022@sanignacio.pr>
2022-02-09 14:50:31 -0400

    Update snake game
    Color pallet.

--- Projects/Alexander-Hinojosa-Sanke.py
+++ Projects/Alexander-Hinojosa-Sanke.py
@@ -1,7 +1,25 @@
 # create the screen
+# Display
+# pygame.init() = inicia todo los terminales de pygame. 
+# pygame.quit() = cierra el programa. 
 import pygame
 pygame.init()
-dis=pygame.display.set_mode((400,300))
-pygame.display.update()
+
+violet= (237, 28, 234) # Score
+green= (101, 250, 95) # Background
+blue= (15, 113, 214) # Fruit 
+orange= (214, 120, 15) # Game over
+yellow= (166, 250, 17) # Snake
+
+dis=pygame.display.set_mode((600,500))
+
+pygame.display.set_caption("Welcome to Snake Game, by 8bit")
+game_over=False
+while not game_over:
+    for event in pygame.event.get():
+        if event.type==pygame.QUIT:
+            game_over=True
+    pygame.draw.rect(dis, yellow,[300, 250 , 20, 20])
+    pygame.display.update()
 pygame.quit()
 quit()
