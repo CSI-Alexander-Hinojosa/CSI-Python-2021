bf9ecdd055d9ecf7ee09d8dfc7a6c3a0214871d9
CSI-Alexander-Hinojosa <25-022@sanignacio.pr>
2022-04-04 14:46:14 -0400

    Creation of Web-Scraping5
    Weather forecast program

---
+++ Projects/Web-Scraping/Alexander-Hinojosa-Web-Scraping5.py
@@ -0,0 +1,19 @@
+from http.client import PRECONDITION_REQUIRED
+import requests
+import bs4
+
+website = requests.get("https://forecast.weather.gov/MapClick.php?lat=41.50730000000004&lon=-87.63386499999996#.Yks1OSjMLIV")
+forecast = bs4.BeautifulSoup(website.content, "html.parser")
+sevenDay = forecast.find(id="seven-day-forecast")
+forecast_items= sevenDay.find_all (class_="tombstone-container")
+tonight = forecast_items[1]
+# print(tonight.prettify())
+
+period= tonight.find(class_="period-name").get_text()
+print(period)
+
+short_des= tonight.find(class_="short-desc").get_text()
+print(short_des)
+
+temp= tonight.find(class_="temp temp-low").get_text()
+print(temp)
